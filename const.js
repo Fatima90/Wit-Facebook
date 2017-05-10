d14a0d677a0f2e1abba01eeff3b44c233403826e
Fatima Hammami <en.f.h@live.com>
2017-05-10 13:12:11 +0300

    heroku

--- const.js
+++ const.js
@@ -1,15 +1,15 @@
 'use strict';
 
 // Wit.ai parameters
-const WIT_TOKEN = process.env.WIT_TOKEN;
+const WIT_TOKEN = "PYXTTSIPB2OKB3D6F4WYGEOXFSGMWIS6";
 if (!WIT_TOKEN) {
   throw new Error('missing WIT_TOKEN');
 }
 
 // Messenger API parameters
-const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+const FB_PAGE_TOKEN = "EAAND1xDzMrYBAHeS9oir5Iktpecy7WmOKcgQVG988HvlOw26nkB2ZAKcrhRQjJ4cxNjuOBVuZASCQPlTx2dXRXMW8YkXA5EcgVlwFmi8rb4Y3WboCMFo0SQZCbDpRnQZBIAydHC5BvxK3lAIDYmyC7lBnL5uteoUmkVXQa0slwZDZD";
 
-var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+var FB_VERIFY_TOKEN = "rebootkamp";
 if (!FB_VERIFY_TOKEN) {
   FB_VERIFY_TOKEN = "just_do_it";
 }
